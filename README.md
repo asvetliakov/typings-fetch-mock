1ffd9d9b28bd9411d9d651dc8b0e198534814cca
Alexey Svetliakov <asvetliakov@teledini.com>
2016-03-08 02:09:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Typed Fetch Mock
+The type definition for [`fetch-mock`](https://github.com/wheresrhys/fetch-mock).
+
+# LICENSE
+MIT
---
+++ index.d.ts
@@ -0,0 +1,45 @@
+type MockMatcherFunction = (url: string, opts: Object) => boolean;
+type MockMatcher = string | RegExp | Function | MockMatcherFunction;
+
+type MockResponse = number | string | Object | MockResponseObject;
+
+type MockResponseFunction = (url: string, opts: Object) => MockResponse;
+
+interface MockResponseObject {
+    body?: string | Object;
+    status?: number;
+    headers?: {[key: string]: any};
+    throws?: boolean;
+    sendAsJson?: boolean;
+}
+
+interface MockCallOptions {
+    [key: string]: any;
+}
+
+type MockCall = [string, RequestInit];
+
+interface MatchedRoutes {
+    matched: Array<MockCall>;
+    unmatched: Array<MockCall>;
+}
+
+interface FetchMockStatic {
+    useNonGlobalFetch(): void;
+    mock(matcher: MockMatcher, response?: MockResponse | MockResponseFunction): this;
+    mock(matcher: MockMatcher, method?: string, response?: MockResponse | MockMatcherFunction): this;
+    restore(): this;
+    reset(): this;
+    reMock(): this;
+
+    calls(matcher?: string): Array<MockCall>
+    calls(): MatchedRoutes;
+    called(matcher?: string): boolean;
+    lastCall(matcher?: string): MockCall;
+    lastUrl(matcher?: string): string;
+    lastOptions(matcher?: string): MockCallOptions;
+
+}
+
+declare var fetchMock: FetchMockStatic;
+export = fetchMock;
